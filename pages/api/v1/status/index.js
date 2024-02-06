e75d2698d963212235ca91d3614ce423ed33c4bd
Solon Gomes Amaral <102386205+solonz@users.noreply.github.com>
2024-02-06 23:21:42 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, response) {
+import database from "../../../../infra/database.js";
+
+async function status(req, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ res: "isso é a uma response" });
 }
 
